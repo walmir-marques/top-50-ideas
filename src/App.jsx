a132517711346a2ff25d9ff3c817e243d373b442
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-04-29 17:28:31 -0300

    basic project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="text-3xl font-bold">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+  );
+};
+export default App;
